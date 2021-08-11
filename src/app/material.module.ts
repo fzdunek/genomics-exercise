40d6f00db038377a047b268497d40b28f96e555f
fzdunek <benfi6-gapfiw-nUrvoc>
2021-08-11 14:58:44 +0200

    [fix] tslint

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -5,7 +5,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from "@angular/material/checkbox";
-import  {MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 
 @NgModule({
   declarations: [],
